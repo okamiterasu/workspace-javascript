7f9d75728165b1f292ecfe1aebbec18ab4bbbe76
Brian Kram <bkram@usca.edu>
2021-10-06 13:14:04 -0400

    Class2

--- js/hello.js
+++ js/hello.js
@@ -1,18 +1,25 @@
-
+'use strict';
 /* Send a customized message based on time
 */
 console.log("I am in the hello.js");
 
 // get the date and time 
-var today = new Date();
-var hoursNow = today.getHours();
+let today = new Date();
+const hoursNow = today.getHours();
 
 // display the greeting message 
 // modify the code so that your code display "Good afternoon" whereas time is from 12:00pm to 6:00pm
-var greetings = "";
-if (hoursNow <= 12){
+let greetings = "";
+if (hoursNow <= 12)
+{
   greetings = "Good Morning";
-}else{
+}
+else if (hoursNow <= 17)
+{
+  greetings = "Good Afternoon";
+}
+else
+{
   greetings = "Good Evening";
 }
 
